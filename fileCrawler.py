791915057b29d93f084d21abc63363001e412fd9
Janek Speit <janek.speit@gmail.com>
2017-05-21 21:24:41 +0200

    added function to filter all links from page

--- fileCrawler.py
+++ fileCrawler.py
@@ -1,2 +1,26 @@
 import urllib2
-data = urllib2.urlopen(url)
+
+def fetch_url(url):
+    response = urllib2.urlopen(url)
+    return response.read()
+
+def cull_links(page):
+    links = []
+    while page.find("href=")  >= 0:
+        ref_mark = page.find("href=") 
+        page = page[ref_mark + 6:]
+        for i in range((len(page)-1)):
+            c = page[i:i+1]
+            if c == "\"" or c == "\'":
+                links.append(page[0:i])
+                page = page[i:]
+                break
+    return links
+
+
+page = fetch_url("https://www.golem.de")
+print (cull_links(page))
+
+
+
+
